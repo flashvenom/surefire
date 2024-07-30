689825d5c84199395e3757d1c91a284c7a6dbd37
flashvenom <flashvenomdesign@gmail.com>
2024-07-29 18:06:42 -0700

    radzen working well

--- Domain/Carriers/Services/CarrierService.cs
+++ Domain/Carriers/Services/CarrierService.cs
@@ -3,21 +3,46 @@ using System.Threading.Tasks;
 using Mantis.Data;
 using Mantis.Domain.Carriers.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace Mantis.Domain.Carriers.Services
 {
     public class CarrierService
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public CarrierService(ApplicationDbContext context)
+        public CarrierService(ApplicationDbContext context, UserManager<ApplicationUser> userManager, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
+            _userManager = userManager;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<List<Carrier>> GetCarriersAsync()
         {
             return await _context.Carriers.ToListAsync();
+        }
+
+        public async Task NewCarrierQuick(Carrier carrier)
+        {
+            var currentUser = await _userManager.GetUserAsync(_httpContextAccessor.HttpContext.User);
+            carrier.CreatedBy = currentUser;
+            //var carrier = new Carrier
+            //{
+            //    CarrierName = fields["CarrierName"],
+            //    StreetAddress = fields["StreetAddress"],
+            //    City = fields["City"],
+            //    State = fields["State"],
+            //    Zip = fields["ZipCode"],
+            //    Phone = fields["Phone"],
+            //    CreatedBy = currentUser
+            //};
+
+            _context.Carriers.Add(carrier);
+            await _context.SaveChangesAsync();
         }
     }
 }
